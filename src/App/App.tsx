e0ed1ae41b9183bc0414187e7429e999076391ad
Анастасия Сапожникова <nastyasap53@gmail.com>
2022-09-19 13:59:29 +0300

    table added

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import {DayleTable} from '../components/Table/DayleTable';
+
+export const App = () => {
+    return (
+        <div className="App">
+            <DayleTable/>
+        </div>
+    );
+}
+
